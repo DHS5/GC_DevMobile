eab79264032ec6b7009a069235cc2c5c9a99cec9
Jérôme <jeromecornu02@gmail.com>
2024-02-19 14:51:44 +0100

    try get touch mobile player controller

--- Assets/2 Dev/1 Gameplay/PlayerController.cs
+++ Assets/2 Dev/1 Gameplay/PlayerController.cs
@@ -1,57 +1,71 @@
 using UnityEngine;
 
-    public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+    [SerializeField] float smoothness = 0.1f;
+    [SerializeField] float leanAngle = 15f;
+    [SerializeField] float leanSpeed = 5f;
+
+    [SerializeField] GameObject model;
+
+    [Header("Camera Bounds")]
+    [SerializeField]
+    Transform cameraFollow;
+
+    [SerializeField] float minX = -3f;
+    [SerializeField] float maxX = 3f;
+    [SerializeField] float minY = -1f;
+    [SerializeField] float maxY = 1.4f;
+    private SpriteRenderer spriteRenderer;
+
+    InputReader input;
+
+    Vector3 currentVelocity;
+    Vector3 targetPosition;
+
+    void Start()
     {
-        [SerializeField] float speed = 4f;
-        [SerializeField] float smoothness = 0.1f;
-        [SerializeField] float leanAngle = 15f;
-        [SerializeField] float leanSpeed = 5f;
-
-        [SerializeField] GameObject model;
-
-        [Header("Camera Bounds")]
-        [SerializeField]
-        Transform cameraFollow;
-
-        [SerializeField] float minX = -3f;
-        [SerializeField] float maxX = 3f;
-        [SerializeField] float minY = -1f;
-        [SerializeField] float maxY = 1.4f;
-
-        InputReader input;
-
-        Vector3 currentVelocity;
-        Vector3 targetPosition;
-
-        void Start()
-        {
-            input = GetComponent<InputReader>();
-        }
-
-        void Update()
-        {
-            targetPosition += new Vector3(input.Move.x, input.Move.y, 0f) * (speed * Time.deltaTime);
-
-            // Calculate the min and max X and Y positions for the player based on the camera view
-            var minPlayerX = cameraFollow.position.x + minX;
-            var maxPlayerX = cameraFollow.position.x + maxX;
-            var minPlayerY = cameraFollow.position.y + minY;
-            var maxPlayerY = cameraFollow.position.y + maxY;
-
-            // Clamp the player's position to the camera view
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPlayerX, maxPlayerX);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPlayerY, maxPlayerY);
-
-            // Lerp the player's position to the target position
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothness);
-
-            // Calculate the rotation effect
-            var targetRotationAngle = -input.Move.x * leanAngle;
-
-            var currentYRotation = transform.localEulerAngles.y;
-            var newYRotation = Mathf.LerpAngle(currentYRotation, targetRotationAngle, leanSpeed * Time.deltaTime);
-
-            // Apply the rotation effect
-            transform.localEulerAngles = new Vector3(0f, newYRotation, 0f);
-        }
+        spriteRenderer = Pool.GetSpriteRenderer();
+        input = GetComponent<InputReader>();
     }
+
+
+
+    void Update()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10f));
+
+        spriteRenderer.Move(targetPosition);
+
+    
+        // DEBUG - to play on pc
+        // ---------------------
+        targetPosition += new Vector3(input.Move.x, input.Move.y, 0f) * (speed * Time.deltaTime);
+
+        // Calculate the min and max X and Y positions for the player based on the camera view
+        var minPlayerX = cameraFollow.position.x + minX;
+        var maxPlayerX = cameraFollow.position.x + maxX;
+        var minPlayerY = cameraFollow.position.y + minY;
+        var maxPlayerY = cameraFollow.position.y + maxY;
+
+        // Clamp the player's position to the camera view
+        targetPosition.x = Mathf.Clamp(targetPosition.x, minPlayerX, maxPlayerX);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, minPlayerY, maxPlayerY);
+
+        // Lerp the player's position to the target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothness);
+
+        // Calculate the rotation effect
+        var targetRotationAngle = -input.Move.x * leanAngle;
+
+        var currentYRotation = transform.localEulerAngles.y;
+        var newYRotation = Mathf.LerpAngle(currentYRotation, targetRotationAngle, leanSpeed * Time.deltaTime);
+
+        // Apply the rotation effect
+        transform.localEulerAngles = new Vector3(0f, newYRotation, 0f);
+        // ---------------------
+    }
+}
