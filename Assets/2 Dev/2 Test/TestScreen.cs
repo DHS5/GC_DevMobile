9ab46ed3df521004a933c3aa7952e650b49bc83d
Tx Mat <46858765+TxMat@users.noreply.github.com>
2024-02-19 14:11:57 +0100

    PS + test screen

--- Assets/2 Dev/2 Test/TestScreen.cs
+++ Assets/2 Dev/2 Test/TestScreen.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _2_Dev._2_Test
 {
@@ -7,18 +8,27 @@ namespace _2_Dev._2_Test
         
         [SerializeField] private SpriteRenderer TL_Point;
         [SerializeField] private SpriteRenderer TR_Point;
-        [SerializeField] private SpriteRenderer Center_Point;
+        [FormerlySerializedAs("Center_Point")] [SerializeField] private SpriteRenderer centerPoint;
         [SerializeField] private SpriteRenderer BL_Point;
         [SerializeField] private SpriteRenderer BR_Point;
+        
+        [FormerlySerializedAs("PointList")] [SerializeField] private SpriteRenderer[] pointList;
         
         private void Start()
         {            
             TL_Point.SetRelativePosition(new Vector2(-1, 1));
-            TR_Point.SetRelativePosition(new Vector2(1, 1));
+            TR_Point.SetRelativePosition(new Vector2(1, 1)); 
             BL_Point.SetRelativePosition(new Vector2(-1, -1));
             BR_Point.SetRelativePosition(new Vector2(1, -1));
             
-            Center_Point.SetRelativePosition(Vector2.zero);
+            centerPoint.SetRelativePosition(Vector2.zero);
+            
+            pointList = new[] {TL_Point, TR_Point, BL_Point, BR_Point, centerPoint};
+            
+            foreach (var point in pointList)
+            {
+                point.SetRelativeSize(1f, 1);
+            }
         }
     }
 }
