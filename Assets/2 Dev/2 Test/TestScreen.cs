016c5a8520e5534cb3eee0df9d87f9dd8bda7c46
Tx Mat <46858765+TxMat@users.noreply.github.com>
2024-02-19 12:07:33 +0100

    test screen

---
+++ Assets/2 Dev/2 Test/TestScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _2_Dev._2_Test
+{
+    public class TestScreen : MonoBehaviour
+    {
+        
+        [SerializeField] private SpriteRenderer TL_Point;
+        [SerializeField] private SpriteRenderer TR_Point;
+        [SerializeField] private SpriteRenderer Center_Point;
+        [SerializeField] private SpriteRenderer BL_Point;
+        [SerializeField] private SpriteRenderer BR_Point;
+        
+        private void Start()
+        {
+            Debug.Log("Screen Resolution: " + Format.Resolution);
+            
+            TL_Point.SetRelativePosition(new Vector2(-1, 1));
+            TR_Point.SetRelativePosition(new Vector2(1, 1));
+            BL_Point.SetRelativePosition(new Vector2(-1, -1));
+            BR_Point.SetRelativePosition(new Vector2(1, -1));
+            
+            Center_Point.SetRelativePosition(Vector2.zero);
+        }
+    }
+}
