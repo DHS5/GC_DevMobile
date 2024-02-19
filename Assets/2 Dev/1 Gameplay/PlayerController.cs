a027665bd4710a3731fc990477c5246cef10bb25
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-02-19 15:30:16 +0100

    Pool Prefab

--- Assets/2 Dev/1 Gameplay/PlayerController.cs
+++ Assets/2 Dev/1 Gameplay/PlayerController.cs
@@ -41,41 +41,4 @@ public class PlayerController : MonoBehaviour
     {
 
     }
-
-    //void Update()
-    //{
-    //    Touch touch = Input.GetTouch(0);
-    //
-    //    targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10f));
-    //
-    //    spriteRenderer.Move(targetPosition);
-    //
-    //
-    //    // DEBUG - to play on pc
-    //    // ---------------------
-    //    targetPosition += new Vector3(input.Move.x, input.Move.y, 0f) * (speed * Time.deltaTime);
-    //
-    //    // Calculate the min and max X and Y positions for the player based on the camera view
-    //    var minPlayerX = cameraFollow.position.x + minX;
-    //    var maxPlayerX = cameraFollow.position.x + maxX;
-    //    var minPlayerY = cameraFollow.position.y + minY;
-    //    var maxPlayerY = cameraFollow.position.y + maxY;
-    //
-    //    // Clamp the player's position to the camera view
-    //    targetPosition.x = Mathf.Clamp(targetPosition.x, minPlayerX, maxPlayerX);
-    //    targetPosition.y = Mathf.Clamp(targetPosition.y, minPlayerY, maxPlayerY);
-    //
-    //    // Lerp the player's position to the target position
-    //    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothness);
-    //
-    //    // Calculate the rotation effect
-    //    var targetRotationAngle = -input.Move.x * leanAngle;
-    //
-    //    var currentYRotation = transform.localEulerAngles.y;
-    //    var newYRotation = Mathf.LerpAngle(currentYRotation, targetRotationAngle, leanSpeed * Time.deltaTime);
-    //
-    //    // Apply the rotation effect
-    //    transform.localEulerAngles = new Vector3(0f, newYRotation, 0f);
-    //    // ---------------------
-    //}
 }
