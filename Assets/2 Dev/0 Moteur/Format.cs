9f6c2478fffc9a5bd781a8967e2010d479a45d05
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-02-19 10:35:18 +0100

    Relative size computation

--- Assets/2 Dev/0 Moteur/Format.cs
+++ Assets/2 Dev/0 Moteur/Format.cs
@@ -33,17 +33,13 @@ public class Format : MonoBehaviour
 
     public static Resolution Resolution => Screen.currentResolution;
 
-    private static float _ratio = 0f;
-    public static float Ratio
-    {
-        get
-        {
-            if (_ratio == 0f) _ratio = Resolution.width / Resolution.height;
-            return _ratio;
-        }
-    }
 
+    public static float Ratio { get; private set; }
 
+    public static float ReferenceRatio { get; private set; }
+    public static float ScaleFactor { get; private set; }
+
+    public static Vector2 ResolutionDelta { get; private set; }
 
     #endregion
 
@@ -51,7 +47,11 @@ public class Format : MonoBehaviour
 
     private void Init()
     {
+        ReferenceRatio = referenceResolution.x / referenceResolution.y;
+        Ratio = Resolution.width / Resolution.height;
 
+        ResolutionDelta = new Vector2(Resolution.width / referenceResolution.x, Resolution.height / referenceResolution.y);
+        ScaleFactor = Mathf.Min(ResolutionDelta.x, ResolutionDelta.y);
     }
 
     #endregion
@@ -60,7 +60,8 @@ public class Format : MonoBehaviour
 
     public static Vector2 ComputeSize(float relativeSize, float ratio)
     {
-        return Vector2.one;
+        float factor = relativeSize * ScaleFactor;
+        return new Vector2(ratio * factor, factor);
     }
 
     #endregion
