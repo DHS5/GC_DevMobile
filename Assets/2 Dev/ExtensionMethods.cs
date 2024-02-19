477ee969015bf245bce553e3a0d233c665f8c100
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-02-19 09:38:06 +0100

    Extension Methods

---
+++ Assets/2 Dev/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    #region Sprite Renderer
+
+    public static void SetRelativeSize(this SpriteRenderer spriteRenderer, Vector2 relativeSize)
+    {
+
+    }
+
+    #endregion
+}
