f9b7dc31fe2f3ddef2a45b912fb3aebdf015dc5f
Jérôme <jeromecornu02@gmail.com>
2024-02-20 17:12:26 +0100

    pause menu

---
+++ Assets/2 Dev/1 Gameplay/UI/PauseMenu.cs
@@ -0,0 +1,46 @@
+using DG.Tweening;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    // public UnityEvent OnPause;
+    // public UnityEvent OnResume;
+
+    private void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        // OnPause.Invoke();
+    }
+
+    private void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+        // OnResume.Invoke();
+    }
+
+    public void TogglePause()
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
