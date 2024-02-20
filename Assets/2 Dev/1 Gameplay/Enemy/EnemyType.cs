9b9af4ce0fa378f361b6a9f161a773b60721522d
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-02-20 09:43:36 +0100

    Enemy movement type

--- Assets/2 Dev/1 Gameplay/Enemy/EnemyType.cs
+++ Assets/2 Dev/1 Gameplay/Enemy/EnemyType.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyType", menuName = "Enemy/EnemyType", order = 0)]
 public class EnemyType : ScriptableObject
 {
+    [SerializeField] private Enemy.EnemyMovement movement;
     [SerializeField] private EnemyPath path;
     [SerializeField] private WeaponStrategy weaponStrategy;
     [SerializeField] private BulletStrategy bulletStrategy;
 
+    public Enemy.EnemyMovement Movement => movement;
     public EnemyPath Path => path;
     public WeaponStrategy WeaponStrategy => weaponStrategy;
     public BulletStrategy BulletStrategy => bulletStrategy;
