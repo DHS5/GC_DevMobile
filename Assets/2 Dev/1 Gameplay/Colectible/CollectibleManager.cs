08d01e667809e85d4adc2ca61c4a96f8460f7b31
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2024-02-21 16:18:33 +0100

    Collectible Manager

--- Assets/2 Dev/1 Gameplay/Colectible/CollectibleManager.cs
+++ Assets/2 Dev/1 Gameplay/Colectible/CollectibleManager.cs
@@ -1,18 +1,72 @@
+using _2_Dev._1_Gameplay.Colectible;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectibleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+
+    private static CollectibleManager I { get; set; }
+
+    private void Awake()
     {
-        
+        if (I != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        I = this;
+
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    [Serializable]
+    private struct CollectibleDataPonderate
     {
-        
+        public CollectibleData collectibleData;
+        [Min(1)] public int weight;
     }
+
+    #region Global Members
+
+    [SerializeField][Range(0f, 1f)] private float collectibleChancePercentage = 0.05f;
+    [SerializeField] private List<CollectibleDataPonderate> collectibleDatas;
+
+    #endregion
+
+    #region Initialization
+
+    private List<CollectibleData> _datas = new();
+
+    private void Init()
+    {
+        foreach (var data in collectibleDatas)
+        {
+            for (int i = 0; i < data.weight; i++)
+            {
+                _datas.Add(data.collectibleData);
+            }
+        }
+    }
+
+    #endregion
+
+    #region Accessor
+
+    private CollectibleData GetRandom()
+    {
+        return _datas[UnityEngine.Random.Range(0, _datas.Count)];
+    }
+    public static CollectibleData Get()
+    {
+        if (UnityEngine.Random.value < I.collectibleChancePercentage)
+            return I.GetRandom();
+        return null;
+    }
+
+    #endregion
 }
