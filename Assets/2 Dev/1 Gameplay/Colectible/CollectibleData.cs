ad565683c1dab910e9571250ec5d9a0ac2061411
Tx Mat <46858765+TxMat@users.noreply.github.com>
2024-02-21 15:26:06 +0100

    CollectibleData.cs

---
+++ Assets/2 Dev/1 Gameplay/Colectible/CollectibleData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _2_Dev._1_Gameplay.Colectible
+{
+    public class CollectibleData : ScriptableObject
+    {
+        [SerializeField] private CollectibleType type;
+        [SerializeField] private float value;
+        [SerializeField] private Sprite sprite;
+
+        public Sprite Sprite => sprite;
+        public CollectibleType Type => type;
+        public float Value => value;
+    }
+}
